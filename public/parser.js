2675b01f7f2cc928f26c76354e344d592e7335c7
alu0100845808 <alu0100845808@ull.edu.es>
2017-04-27 18:28:46 +0000

    analizar drp acabado -> objetivo revisar

--- public/parser.js
+++ public/parser.js
@@ -34,18 +34,21 @@
       ADDOP: /[+-]/g,
       MULTOP: /[*\/]/g
     };
+    
     RESERVED_WORD = {
       p: "P",
       "if": "IF",
       "then": "THEN",
-      "else": "ELSE"
+      "else": "ELSE",
+      "const": "CONST",
+      "for": "FOR"
     };
     
     BOOLEAN = {
       "true": "TRUE",
       "false": "FALSE"
     };
-    
+
     make = function(type, value) {
       return {
         type: type,
@@ -54,12 +57,14 @@
         to: i
       };
     };
+    
     getTok = function() {
       var str;
       str = m[0];
       i += str.length;
       return str;
     };
+    
     if (!this) {
       return;
     }
@@ -92,6 +97,10 @@
         result.push(make("STRING", getTok().replace(/^["']|["']$/g, "")));
       } else if (m = tokens.COMPARISONOPERATOR.bexec(this)) {
         result.push(make("COMPARISON", getTok()));
+      }else if (m = tokens.TWOCHAROPERATORS.bexec(this)) {
+        result.push(make("TWOCHAROPERATOR", getTok()));
+      } else if (m = tokens.ONECHAROPERATORS.bexec(this)) {
+        result.push(make(m[0], getTok()));
       } else if (m = tokens.ADDOP.bexec(this)) {
         result.push(make("ADDOP", getTok()));
       } else if (m = tokens.MULTOP.bexec(this)) {
@@ -106,7 +115,8 @@
   };
 
   var parse = function(input) {
-    var condition, expression, factor, lookahead, match, statement, statements, term, tokens, tree;
+    var table = [];
+    var inc, consts, asignation, funcion, condition, comp, conditional, bucle, expression, factor, lookahead, match, statement, statements, term, tokens, tree, comma;
     tokens = input.tokens();
     lookahead = tokens.shift();
     match = function(t) {
@@ -120,62 +130,276 @@
       }
     };
     
-    expression = function() {
-      var result, right, type;
-      result = term();
-      while (lookahead && lookahead.type === "ADDOP") {
-        type = lookahead.value;
-        match("ADDOP");
-        right = term();
-        result = {
-          type: type,
-          left: result,
-          right: right
-        };
+    comma = function() {
+      var array_result = [];
+      if (lookahead.type === "FOR")
+          array_result.push(bucle());
+      else array_result.push(expression());
+      while (lookahead && lookahead.type == ";") {
+        match(";");
+        if (lookahead.type === "FOR")
+            array_result.push(bucle());
+        else array_result.push(expression());
       }
-      return result;
-    };
-    term = function() {
-      var result, right, type;
-      result = factor();
-      while (lookahead && lookahead.type === "MULTOP") {
-        type = lookahead.value;
-        match("MULTOP");
-        right = factor();
-        result = {
-          type: type,
-          left: result,
-          right: right
-        };
-      }
-      return result;
-    };
-    factor = function() {
-      var result;
-      result = null;
-      if (lookahead.type === "NUM") {
-        result = {
-          type: "NUM",
-          value: lookahead.value
-        };
-        match("NUM");
-      } else if (lookahead.type === "ID") {
-        result = {
-          type: "ID",
-          value: lookahead.value
-        };
-        match("ID");
-      } else if (lookahead.type === "(") {
-        match("(");
-        result = expression();
-        match(")");
-      } else {
-        throw "Syntax Error. Expected number or identifier or '(' but found " + (lookahead ? lookahead.value : "end of input") + " near '" + input.substr(lookahead.from) + "'";
-      }
-      return result;
-    };
+      return array_result;
 
-    tree = expression(input);
+  }
+
+  expression = function() {
+    let result, right, type;
+    let args = [];
+    if (lookahead.type === "IF")
+        return conditional();
+    if (lookahead && lookahead.type == "CONST") {
+        return consts();
+    }
+      result = term();
+      if (lookahead && lookahead.value === "(" && result.type === "ID") {
+        match("(");
+        args.push(factor());
+        while (lookahead.value === ",") {
+          match(",");
+          args.push(term());
+        }
+        match(")");
+        result = {
+           type: "ID",
+           value: result.value,
+           argumentos: args
+        };
+      }
+      else if (lookahead && lookahead.type === "=" && result.type === "ID") {
+        result = asignation(result);
+    } else {
+        while(lookahead && lookahead.type === "ADDOP"){
+          type = lookahead.value;
+          match("ADDOP");
+          right = term();
+          result = {
+             type: type,
+             left: result,
+             right: right
+          };
+         }
+      }
+
+    if(lookahead && lookahead.type === "COMPARISON") {
+      result = comp(result);
+    }
+
+    return result;
+  };
+
+   bucle = function() {
+     let init, condition, result, increment, code;
+     match("FOR");
+     match("(");
+     init = term();
+     if (lookahead && lookahead.type === "=" && init.type === "ID") {
+       init = asignation(init);
+     }
+     match(";");
+     condition = term();
+     if (lookahead && lookahead.type === "COMPARISON" && condition.type === "ID") {
+       condition = comp(condition);
+     }
+     match(";");
+     increment = inc();
+     match(")");
+     match("{");
+     if (lookahead && lookahead.value != "}") {
+       code = comma();
+     }
+     match("}");
+     result = {
+       type: "FOR",
+       inicio: init,
+       condicion: condition,
+       inc: increment,
+       codigo: code
+     }
+     return result;
+   }
+
+   inc = function() {
+     var operator, id, increment;
+     id = term();
+     if (lookahead && lookahead.type === "TWOCHAROPERATOR" && id.type === "ID") {
+       operator = lookahead.value;
+       match("TWOCHAROPERATOR");
+       if (operator === "++")
+        increment = 1;
+       else if (operator === "--") {
+         increment = -1;
+       }
+     }
+     return increment;
+   }
+   
+   consts = function() {
+     match("CONST");
+     let result;
+     result = term();
+     if (lookahead && lookahead.type === "=" && result.type === "ID") {
+       if (!table[result.value])
+         table.push(result.value);
+       result.consts = true;
+       result = asignation(result);
+     }
+     return result;
+   }
+
+  asignation = function(id) {
+    var assignment;
+    let result;
+    match("=");
+    if (lookahead.value == "->") {
+      match("->");
+      result = funcion();
+    }
+     else {
+       assignment = expression();
+       result = {
+           type: "=",
+           left: id,
+           right: assignment
+         };
+     }
+    return result;
+  }
+
+  funcion = function(id) {
+    let args = [];
+    let code;
+    let j = 0;
+    let statement;
+    let funcionArray;
+    let result;
+    match("(");
+    args.push(lookahead.value);
+    match("ID");
+    while (lookahead && lookahead.value == ",") {
+      match(",");
+      args.push(lookahead.value);
+      match("ID");
+    }
+    match(")");
+    match("{");
+    if (lookahead && lookahead.value != "}") {
+      code = comma();
+    }
+    match("}");
+
+    result = {
+        nombrefuncion: id,
+        argumentos: args,
+        code: code
+      }
+
+      return result
+    }
+
+
+  conditional = function() {
+      let condicion, resultado, resultado1, resultado2;
+      match("IF");
+      condicion = expression();
+      match("THEN");
+      resultado1 = expression();
+      match("ELSE");
+      resultado2 = expression();
+      resultado = {
+           type: "IF",
+           condition: condicion,
+           type2: "THEN",
+           first: resultado1,
+           type3:  "ELSE",
+           second: resultado2,
+
+         };
+      return resultado;
+  }
+
+   comp = function(left) {
+    var comparator = lookahead.value;
+    var resultado;
+    match("COMPARISON");
+    right = expression();
+    resultado = {
+       type: comparator,
+       left: left,
+       right: right
+     };
+    return resultado;
+  }
+  term = function() {
+    var result, right, type;
+    result = factor();
+    if (lookahead && lookahead.type === "MULTOP") {
+      type = lookahead.value;
+      match("MULTOP");
+      right = term();
+      if(type === "*"){
+        result = {
+          type: "*",
+          left: result,
+          right: right
+        };
+      }
+      else if(type === "/"){
+        result = {
+          type: "/",
+          left: result,
+          right: right
+        };
+      }
+    }
+    return result;
+  };
+
+  factor = function() {
+    var result, id;
+    result = null;
+    if (lookahead.type === "NUM") {
+      result = {
+       type: "NUM",
+       value: lookahead.value
+     };
+        match("NUM");
+    }
+      else if(lookahead.type === "ID") {
+        id = lookahead.value;
+        if (table.indexOf(id) > -1) {
+            result = {
+             type: "ID",
+             value: id,
+             consts: true
+           };
+         } else {
+           result = {
+            type: "ID",
+            value: id,
+            consts: false
+          };
+          }
+            match("ID");
+    } else if (lookahead.type === "BOOLEAN") {
+      result = {
+       type: "BOOLEAN",
+       value: lookahead.value
+     };
+      match("BOOLEAN");
+    }else if (lookahead.type === "(") {
+      match("(");
+      result = expression();
+      match(")");
+    }else {
+      throw "Syntax Error. Expected number or identifier or '(' but found " + (lookahead ? lookahead.value : "end of input") + " near '" + input.substr(lookahead.from) + "'";
+    }
+    return result;
+  };
+
+    tree = comma(input);
     if (lookahead != null) {
       throw "Syntax Error parsing statements. " + "Expected 'end of input' and found '" + input.substr(lookahead.from) + "'";
     }
